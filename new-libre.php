a5e854e8c35c8fbf99517cb6f47394cc2f2f781c
Sara <sara.mirabel@yahoo.fr>
2018-12-05 20:50:01 +0100

    Scénario libre entre 2 bugs

--- new-libre.php
+++ new-libre.php
@@ -21,19 +21,25 @@ include_once("layout/header.php");
 	<img class="icon-projet" src="img/edit.svg">
 	<div class="content">
 		<p id="bold-color">Format libre</p>
-		<p>Le scénario libre vous permet de créer votre propre histoire avec un nombre de plans illimité. Choisissez le temps, rythmez votre vidéo avec de la musique, et devenez maître de votre vidéo.</p>
-		<p class="formulaire">Nom du projet</p>
-		<p>Format</p>
+		<p style="margin-bottom: 60px;">Le scénario libre vous permet de créer votre propre histoire avec un nombre de plans illimité. Choisissez le temps, rythmez votre vidéo avec de la musique, et devenez maître de votre vidéo.</p>
+		<form>
+			<p>
+				<input type="text" class="formulaire" name="Nom du projet" value="Nom du projet"/>
+			</p>
+		</form>
+		<p class="formulaire-title">Format</p>
 		<div style="display: flex;">
 			<p class="formulaire-round">minutes</p>
 			<p class="formulaire-round1">secondes</p>
 		</div>
-		<p>Durée</p>
+		<p class="formulaire-title">Durée</p>
 		<div style="display: flex;">
 			<p class="formulaire-round">vertical</p>
 			<p class="formulaire-round1">horizontal</p>
 		</div>
-		<p class="btn">commencer</p>
+		<div style="display: flex;">
+			<p class="btn-formulaire">commencer</p>
+		</div>
 	</div>
 </section>
 
